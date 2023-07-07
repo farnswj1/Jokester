1ffbd539e0dd13a5e24f82ceaaa5cdd36158eb6c
Justin Farnsworth <farnswj1@tcnj.edu>
2023-07-07 00:00:23 -0400

    moved protected route component to components directory

--- frontend/src/auth/index.tsx
+++ frontend/src/auth/index.tsx
@@ -1,4 +1,3 @@
 export { default as AuthProvider } from './AuthProvider';
-export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as TokenHandler } from './TokenHandler';
 export { default as User } from './User';
