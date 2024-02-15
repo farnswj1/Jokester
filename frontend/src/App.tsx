368021d43b005ad6abb4469f5071bb3e541aecd7
Justin Farnsworth <farnswj1@tcnj.edu>
2024-02-15 14:11:32 -0500

    add CustomThemeProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,25 @@
-import { FC, useEffect, useState } from 'react';
-import {
-  Box,
-  createTheme,
-  CssBaseline,
-  PaletteMode,
-  useMediaQuery,
-  ThemeProvider
-} from '@mui/material';
-import { deepPurple } from '@mui/material/colors';
+import { FC } from 'react';
+import { Box } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from 'auth';
-import { SnackbarProvider } from 'providers';
-import { PaletteModeContext } from 'contexts';
+import { CustomThemeProvider, SnackbarProvider } from 'providers';
 import { Header, Footer } from 'layouts';
 import Routes from 'routes';
 
-const App: FC = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  let initialMode = localStorage.getItem('mode') as PaletteMode | null;
-
-  if (initialMode === null) {
-    initialMode = prefersDarkMode ? 'dark' : 'light';
-  }
-
-  const [paletteMode, setPaletteMode] = useState<PaletteMode>(initialMode);
-  useEffect(() => localStorage.setItem('mode', paletteMode), [paletteMode]);
-
-  const theme = createTheme({
-    palette: {
-      mode: paletteMode,
-      primary: {
-        main: deepPurple[500]
-      },
-      background: {
-        default: paletteMode === 'dark' ? '#343434' : '#dddddd'
-      }
-    }
-  });
-
-  return (
-    <PaletteModeContext.Provider value={setPaletteMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <BrowserRouter>
-          <AuthProvider>
-            <SnackbarProvider defaultProps={{ autoHideDuration: 5000 }}>
-              <Header />
-              <Box component="main" paddingY={5} marginBottom="auto">
-                <Routes />
-              </Box>
-              <Footer />
-            </SnackbarProvider>
-          </AuthProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </PaletteModeContext.Provider>
-  );
-};
+const App: FC = () => (
+  <CustomThemeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <SnackbarProvider defaultProps={{ autoHideDuration: 5000 }}>
+          <Header />
+          <Box component="main" paddingY={5} marginBottom="auto">
+            <Routes />
+          </Box>
+          <Footer />
+        </SnackbarProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </CustomThemeProvider>
+);
 
 export default App;
--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -1,1 +1,2 @@
+export { default as CustomThemeProvider } from './CustomThemeProvider';
 export { default as SnackbarProvider } from './SnackbarProvider';
